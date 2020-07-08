e93001074958dbecd8eff7b33e840901cb0c63d5
Matt_Bundner <mbundner13@gmail.com>
2020-07-08 12:41:12 -0400

    added button to rest of images

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 let topRow = document.querySelectorAll(".top")
-
-console.log(topRow)
+//console.log(topRow)
 
 for(let i=0; i<topRow.length; i++){
     button = document.createElement("button")
@@ -8,6 +7,27 @@ for(let i=0; i<topRow.length; i++){
     button.innerText = "name"
     topRow[i].appendChild(br)
     topRow[i].appendChild(button)
-    
+}
 
+let middleRow = document.querySelectorAll(".middle")
+//console.log(middleRow)
+
+for(let i=0; i<middleRow.length; i++){
+    button = document.createElement("button")
+    let br = document.createElement("br")
+    button.innerText = "name"
+    middleRow[i].appendChild(br)
+    middleRow[i].appendChild(button)
+}
+    
+let bottomRow = document.querySelectorAll(".bottom")
+//console.log(bottomRow)
+
+for(let i=0; i<bottomRow.length; i++){
+    button = document.createElement("button")
+    let br = document.createElement("br")
+    button.innerText = "name"
+    bottomRow[i].appendChild(br)
+    bottomRow[i].appendChild(button)
+    
 }
