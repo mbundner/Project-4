521ac2e0ac4a44017ad852a0571735100c3d1ea0
Matt_Bundner <mbundner13@gmail.com>
2020-07-08 16:04:02 -0400

    created function for self-scoring

--- script.js
+++ script.js
@@ -1,9 +1,63 @@
-let topRow = document.querySelectorAll(".top")
-//console.log(topRow)
+const names = ["Bart Simpson", "Marge Simpson", "Montgomery Burns", "Maggie Simpson", "Ned Flanders",
+               "Jimbo Jones", "Milhouse", "Troy McClure", "Apu", "Reverend Lovejoy", "Barney", "Chief Wiggum",
+               "Princical Skinner", "Smithers", "Sideshow Bob", "Abe Simpson","Bleeding Gums Murphy",
+               "Homer Simpson", "Nelson Muntz" ]
 
-for(let i=0; i<topRow.length; i++){
-    button = document.createElement("button")
+
+
+
+
+
+
+
+
+/*
+let topRow = document.querySelector(".top")
+//console.log(topRow)
+    
+let button = document.createElement("button")
     let br = document.createElement("br")
+    button.classList.add("button")
+    button.innerText = "who is this?"
+    topRow.appendChild(br)
+    topRow.appendChild(button)
+    let br2 = document.createElement("br")
+    
+    
+   
+    button.addEventListener("click", revealName)
+    
+    function revealName(e){
+      e.preventDefault()
+      p = document.createElement("paragraph")
+      p.classList.add("text")
+      p.innerText = names[0]
+      topRow.appendChild(br2)
+      topRow.appendChild(p)
+      
+    }
+
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+for(let i=0; i<topRow.length; i++){
+    let button = document.createElement("button")
+    let br = document.createElement("br")
+    button.classList.add("button")
     button.innerText = "name"
     topRow[i].appendChild(br)
     topRow[i].appendChild(button)
@@ -13,8 +67,9 @@ let middleRow = document.querySelectorAll(".middle")
 //console.log(middleRow)
 
 for(let i=0; i<middleRow.length; i++){
-    button = document.createElement("button")
+    let button = document.createElement("button")
     let br = document.createElement("br")
+    button.classList.add("button")
     button.innerText = "name"
     middleRow[i].appendChild(br)
     middleRow[i].appendChild(button)
@@ -24,10 +79,25 @@ let bottomRow = document.querySelectorAll(".bottom")
 //console.log(bottomRow)
 
 for(let i=0; i<bottomRow.length; i++){
-    button = document.createElement("button")
+    let button = document.createElement("button")
     let br = document.createElement("br")
+    button.classList.add("button")
     button.innerText = "name"
     bottomRow[i].appendChild(br)
     bottomRow[i].appendChild(button)
     
+}
+*/
+
+let correct = document.querySelector("#correct")
+//console.log(correct)
+
+correct.addEventListener("click", incrementUp)
+
+function incrementUp(e){
+    e.preventDefault()
+    score = document.querySelector("#score").innerHTML
+    score++
+    document.querySelector("#score").innerHTML = score
+
 }
