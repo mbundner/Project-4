6253443ef0a723f270ebe0963a90900654647127
Matt_Bundner <mbundner13@gmail.com>
2020-07-08 20:17:00 -0400

    added final characters

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-const names = ["Bart Simpson", "Marge Simpson", "Montgomery Burns", "Maggie Simpson", "Ned Flanders",
-               "Jimbo Jones", "Milhouse", "Troy McClure", "Apu", "Reverend Lovejoy", "Barney", "Chief Wiggum",
-               "Princical Skinner", "Lisa", "Smithers", "Sideshow Bob", "Abe Simpson","Bleeding Gums Murphy",
-               "Homer Simpson", "Nelson Muntz" ]
+const names = ["Bart Simpson", "Marge Simpson", "Montgomery Burns", "Maggie Simpson", "Ned Flanders","Jimbo Jones", "Milhouse Van Houten", "Kearney",
+               "Moe Szyslak", "Groundskeeper Willie", "Mayor Quimby", "Lenny Leonard", "Kent Brockman", "Ralph Wiggum", "Martin Prince, Jr",
+               "Troy McClure", "Apu Nahasap...", "Reverend Lovejoy", "Barney Gumble", "Chief Wiggum","Princical Skinner", "Lisa Simpsons", 
+               "Waylon Smithers", "Sideshow Bob", "Abe Simpson","Bleeding Gums Murphy","Homer Simpson", "Nelson Muntz", "Krusty The Clown", "Herb Powell"]
 
 let images = document.querySelectorAll(".image")
 //console.log(images)
@@ -22,74 +22,7 @@ for(let i=0; i<images.length; i++){
     
 }
     
-/*
-images[i].addEventListener("click", revealName)
-      function revealName(e){
-          e.preventDefault()
-          console.log(e)
 
-*/
-
-
-
-
-   /*
-
-    
-    
-*/
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*
-for(let i=0; i<topRow.length; i++){
-    let button = document.createElement("button")
-    let br = document.createElement("br")
-    button.classList.add("button")
-    button.innerText = "name"
-    topRow[i].appendChild(br)
-    topRow[i].appendChild(button)
-}
-
-let middleRow = document.querySelectorAll(".middle")
-//console.log(middleRow)
-
-for(let i=0; i<middleRow.length; i++){
-    let button = document.createElement("button")
-    let br = document.createElement("br")
-    button.classList.add("button")
-    button.innerText = "name"
-    middleRow[i].appendChild(br)
-    middleRow[i].appendChild(button)
-}
-    
-let bottomRow = document.querySelectorAll(".bottom")
-//console.log(bottomRow)
-
-for(let i=0; i<bottomRow.length; i++){
-    let button = document.createElement("button")
-    let br = document.createElement("br")
-    button.classList.add("button")
-    button.innerText = "name"
-    bottomRow[i].appendChild(br)
-    bottomRow[i].appendChild(button)
-    
-}
-*/
-/*
 let correct = document.querySelector("#correct")
 //console.log(correct)
 
@@ -102,4 +35,3 @@ function incrementUp(e){
     document.querySelector("#score").innerHTML = score
     alert("You guessed right!")
 }
-*/
